c9b558650e7cfabd360512f3e57d31e8e455c3c6
przempol <przempol777@gmail.com>
2020-01-19 18:42:21 +0100

    Added lab 11 homework

--- lab_11/tasks/tests/tests_task1.py
+++ lab_11/tasks/tests/tests_task1.py
@@ -7,3 +7,52 @@ from lab_11.tasks.tools.calculator import (
     NotNumberArgument,
     WrongOperation,
 )
+
+
+@pytest.fixture
+def calculator():
+    return Calculator()
+
+
+@pytest.mark.parametrize("operator,arg1,arg2,expected",
+                         [("+", 2, 3, 5),
+                          ("-", 2, 5, -3),
+                          ("*", 2, 3, 6),
+                          ("/", 3, 2, 1.5),
+                          ])
+def test_run_valid_data(operator, arg1, arg2, expected, calculator):
+    assert calculator.run(operator, arg1, arg2) == expected
+
+
+@pytest.mark.parametrize("operate,arg1,expected",
+                         [('+', 3, 6),
+                          ('-', 3, 0),
+                          ('/', 3, 1),
+                          ('*', 3, 9),
+                          ])
+def test_memorize(calculator, operate, arg1, expected):
+    calculator.run('+', 2, 1)
+    assert calculator._short_memory == 3
+
+    # set number 3 to memory
+    calculator.memorize()
+    assert calculator.memory == 3
+
+    assert calculator.run(operate, arg1) == expected
+
+    calculator.clean_memory()
+    with pytest.raises(EmptyMemory):
+        calculator.in_memory()
+
+
+@pytest.mark.parametrize("operator,arg1,arg2,expected",
+                         [("+", 1, "b", NotNumberArgument),
+                          ("-", "a", 1, NotNumberArgument),
+                          ("-", "a", "b", NotNumberArgument),
+                          ("^", 1, 2, WrongOperation),
+                          ("/", 1, 0, CalculatorError),
+                          ("/", 2, None, EmptyMemory),
+                          ])
+def test_run_invalid_data(operator, arg1, arg2, expected, calculator):
+    with pytest.raises(expected):
+        calculator.run(operator, arg1, arg2)
--- lab_11/tasks/tests/tests_task2.py
+++ lab_11/tasks/tests/tests_task2.py
@@ -1,5 +1,44 @@
 import pytest
+import requests
+import requests_mock
 
 from lab_11.tasks.tools.metaweather import (
-    get_metaweather
+    get_metaweather,
+    get_cities_woeid
 )
+
+
+location_url = 'https://www.metaweather.com/api/location/search'
+
+
+@pytest.fixture()
+def mock():
+    with requests_mock.Mocker() as m:
+        yield m
+
+
+def test_json_empty(mock):
+    mock.get(location_url, json=[])
+    assert get_cities_woeid('Warszawa') == {}
+
+
+def test_json(mock):
+    mock.get(location_url, json=[
+        {'title': 'Warsaw', 'woeid': 523920},
+        {'title': 'Newark', 'woeid': 2459269}
+    ])
+    assert get_cities_woeid('War') == {
+        'Warsaw': 523920,
+        'Newark': 2459269,
+    }
+
+
+def test_timeout():
+    with pytest.raises(requests.exceptions.Timeout):
+        get_cities_woeid('War', timeout=0.01)
+
+
+def test_404_exception(mock):
+    mock.get(location_url, status_code=404)
+    with pytest.raises(requests.exceptions.HTTPError):
+        get_cities_woeid('War')
